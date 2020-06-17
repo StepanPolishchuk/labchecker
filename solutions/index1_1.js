bc8044b8f33c7c99abf2121e68a918bd4eb4110b
StepanPolishchuk <65761144+StepanPolishchuk@users.noreply.github.com>
2020-06-17 10:39:12 +0300

    Add files via upload

---
+++ solutions/index1_1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const fn = () => {
+  const a = 5;
+  console.log({ a });
+
+};
+
+module.exports = { fn };
---
+++ solutions/index2_1.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const name = 'Stepan';
+
+module.exports = { name };
